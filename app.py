5a56e2bd6fcfebd71f38022ece213662e6bb7241
almeidava93 <almeida.va93@gmail.com>
2023-02-03 18:29:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,32 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return """
+        <html>
+        <head>
+            <style>
+            body {
+                background-color: #f2f2f2;
+                font-family: Arial, sans-serif;
+                text-align: center;
+            }
+
+        h1 {
+            font-size: 48px;
+            color: #333;
+            margin-top: 50px;
+        }
+        
+        p {
+            font-size: 18px;
+            color: #555;
+            margin-top: 20px;
+        }
+        </style>
+        </head>
+        <body>
+            <h1>Welcome!</h1>
+            <p>We are happy to have you here.</p>
+        </body>
+        </html>
+            """
